4106c72ca364409a4c227181178036302a84c9ac
darulgusti <darulgusti@example.com>
2025-12-25 16:47:52 +0700

    update 2

--- js/services/PrayerService.js
+++ js/services/PrayerService.js
@@ -1,20 +1,22 @@
 class PrayerService {
-            constructor() {
-                this.prayers = [
-                    { name: "Subuh", time: "04:22" },
-                    { name: "Dzuhur", time: "11:54" },
-                    { name: "Ashar", time: "15:20" },
-                    { name: "Maghrib", time: "18:05" },
-                    { name: "Isya", time: "19:15" }
-                ];
-            }
+    constructor() {
+        // Default prayer times for Malang (data tetap)
+        // Updated: Using data for Malang, East Java (-7.9797, 112.6304)
+        this.prayers = [
+            { name: "Subuh", time: "04:32" },
+            { name: "Dzuhur", time: "12:14" },
+            { name: "Ashar", time: "15:38" },
+            { name: "Maghrib", time: "18:19" },
+            { name: "Isya", time: "19:32" }
+        ];
+    }
 
-            getSchedule() {
-                return this.prayers;
-            }
+    getSchedule() {
+        return this.prayers;
+    }
 
-            getNextPrayer() {
-                // Dalam aplikasi nyata, ini akan membandingkan dengan waktu sekarang
-                return this.prayers[2]; // Mockup: Ashar
-            }
-        }
+    getNextPrayer() {
+        // Dalam aplikasi nyata, ini akan membandingkan dengan waktu sekarang
+        return this.prayers[2]; // Mockup: Ashar
+    }
+}
